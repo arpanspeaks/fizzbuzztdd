08e99978e076f0983676cf2c360d76808c3e671a
Arpan Sardar <arpanspeaks@gmail.com>
2025-04-11 11:04:10 +0530

    project initialization

---
+++ src/test/java/com/example/fizzbuzztdd/FizzbuzztddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fizzbuzztdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizzbuzztddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
